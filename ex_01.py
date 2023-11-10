c5beec728e9dd92b315aa88963bf8ef1e1d4ca2a
Nike <nikechukwu@gmail.com>
2023-11-10 09:29:27 +0100

    Exercise 01 Code committed

---
+++ ex_01.py
@@ -0,0 +1,5 @@
+first_name = input("Please enter first name: ")
+last_name = input("Please enter last name: ")
+age = input("Please enter age: ")
+
+print("Hello,", first_name, last_name + ". You are", age ,"years old.")
